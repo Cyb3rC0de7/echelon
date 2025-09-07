99a36b77b8f3846cb3d1de2133cd3580329496ba
Qwinton Knocklein <u21669849@tuks.co.za>
2025-09-07 23:12:51 +0200

    Added the route wildcard back to index.js to see if that fixes Heroku routing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -25,7 +25,7 @@ api.interceptors.response.use(
     if (error.response && error.response.status === 401) {
       localStorage.removeItem('authToken');
       localStorage.removeItem('user');
-      window.location.href = '/login';
+      window.location.href = '/';
     }
     return Promise.reject(error);
   }
--- server/src/index.js
+++ server/src/index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(buildPath));
   
   // Handle React routing - send all non-API requests to index.html
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 } else {
